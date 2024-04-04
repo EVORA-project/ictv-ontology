03ab1e4cf090573ebdd97109df6dd1ed9e4ac058
James McLaughlin <james@mclgh.net>
2024-04-04 23:37:19 +0100

    update rdf script for molecule types

--- create_rdf.py
+++ create_rdf.py
@@ -11,13 +11,33 @@ OBSOLESCENCE_REASON = "http://purl.obolibrary.org/obo/IAO_0000225"
 TERM_SPLIT = "http://purl.obolibrary.org/obo/IAO_0000229"
 TERMS_MERGED = "http://purl.obolibrary.org/obo/IAO_0000227"
 
+prefix = 'http://ictv.global/'
+
+
 def main():
+
+    molecules = pd.read_csv('data/taxonomy_molecule.tsv', sep='\t', dtype=str)
+# id	abbrev	name	balt_group	balt_roman	description	left_idx	right_idx
+
+    common_graph = rdflib.Graph()
+
+    molecule_superclass_iri = prefix + 'molecule'
+    common_graph.add((URIRef(molecule_superclass_iri), RDF.type, OWL.Class))
+    common_graph.add((URIRef(molecule_superclass_iri), RDFS.label, Literal("Molecule")))
+    common_graph.add((URIRef(molecule_superclass_iri), RDFS.subClassOf, OWL.Thing))
+
+    for molecule in molecules.itertuples():
+        molecule_iri = prefix + 'molecule_' + molecule.id
+        common_graph.add((URIRef(molecule_iri), RDF.type, OWL.Class))
+        common_graph.add((URIRef(molecule_iri), RDFS.subClassOf, URIRef(molecule_superclass_iri)))
+        common_graph.add((URIRef(molecule_iri), RDFS.label, Literal(molecule.abbrev)))
+        common_graph.add((URIRef(molecule_iri), RDFS.comment, Literal(molecule.name)))
+
     nodes = pd.read_csv('data/taxonomy_node.tsv', sep='\t', on_bad_lines=lambda x: x[:-1], engine='python', dtype=str)
     delta = pd.read_csv('data/taxonomy_node_delta.tsv', sep='\t', dtype=str)
 
+    # releases = nodes[(nodes['level_id'] == '100') & (nodes['name'] != 'empty_tree') & (nodes['name'] == '2014')]
     releases = nodes[(nodes['level_id'] == '100') & (nodes['name'] != 'empty_tree')]
-
-    prefix = 'http://ictv.global/'
 
     for release in releases.itertuples():
         print(f'Processing release {release.name}')
@@ -28,7 +48,7 @@ def main():
 
         nodes_in_release = nodes[(nodes['msl_release_num'] == release_num) & (nodes['level_id'] != '100')]
 
-        g = rdflib.Graph()
+        g = rdflib.Graph() + common_graph
         g.add((URIRef(ontology_iri), RDF.type, OWL.Ontology))
 
         for node in nodes_in_release.itertuples():
@@ -40,12 +60,12 @@ def main():
             if not replacements.empty:
                 is_merged = replacements['is_merged'].value_counts().get('1', 0) > 0
                 is_split = replacements['is_split'].value_counts().get('1', 0) > 0
-                is_moved = replacements['is_moved'].value_counts().get('1', 0) > 0
-                is_promoted = replacements['is_promoted'].value_counts().get('1', 0) > 0
-                is_demoted = replacements['is_demoted'].value_counts().get('1', 0) > 0
-                is_renamed = replacements['is_renamed'].value_counts().get('1', 0) > 0
-                is_new = replacements['is_new'].value_counts().get('1', 0) > 0
-                is_deleted = replacements['is_deleted'].value_counts().get('1', 0) > 0
+                # is_moved = replacements['is_moved'].value_counts().get('1', 0) > 0
+                # is_promoted = replacements['is_promoted'].value_counts().get('1', 0) > 0
+                # is_demoted = replacements['is_demoted'].value_counts().get('1', 0) > 0
+                # is_renamed = replacements['is_renamed'].value_counts().get('1', 0) > 0
+                # is_new = replacements['is_new'].value_counts().get('1', 0) > 0
+                # is_deleted = replacements['is_deleted'].value_counts().get('1', 0) > 0
 
                 for replacement in replacements.itertuples():
                     if not pd.isna(replacement.new_taxid):
@@ -58,9 +78,15 @@ def main():
 
             g.add((URIRef(class_iri), RDF.type, OWL.Class))
             g.add((URIRef(class_iri), RDFS.label, Literal(node.name)))
-            if node.parent_id != release.ictv_id:
-                g.add((URIRef(class_iri), RDFS.subClassOf, URIRef(f'{prefix}ictv_{node.parent_id}')))
-            g.add((URIRef(class_iri), RDFS.isDefinedBy, URIRef(ontology_iri)))
+            if node.parent_id != release.taxnode_id:
+                g.add((URIRef(class_iri), RDFS.subClassOf, URIRef(f'{prefix}{node.parent_id}')))
+
+            # g.add((URIRef(class_iri), RDFS.isDefinedBy, URIRef(ontology_iri)))
+
+            if not pd.isna(node.molecule_id):
+                g.add((URIRef(class_iri), RDFS.subClassOf, URIRef(f'{prefix}molecule_{node.molecule_id}')))
+
+            molecule_iri = prefix + 'molecule_' + molecule.id
 
         g.bind('owl', OWL)
         g.bind('iao', 'http://purl.obolibrary.org/obo/IAO_')
@@ -68,38 +94,6 @@ def main():
 
 
 
-def map_molecule(mol_id):
-    if mol_id == '0':
-        return None
-    elif mol_id == '1': # dsDNA	Double-stranded DNA	1	I	NULL	20	30
-        return "http://purl.obolibrary.org/obo/SO_0001198" # ds_DNA_viral_sequence
-
-    elif mol_id == '2':	# ssDNA	Single-stranded DNA	2	II	NULL	100	800
-        print("todo")
-        exit(1)
-        return 
-
-    elif mol_id == '3':	# dsRNA	Double-stranded RNA	3	III	NULL	NULL	NULL
-        return "http://purl.obolibrary.org/obo/SO_0001169" # ds_RNA_viral_sequence
-
-    elif mol_id == '4':	# ssRNA(+)	Single-stranded RNA - Positive-sense	4	IV	NULL	1300	1400
-        return "http://purl.obolibrary.org/obo/SO_0001201" # positive_sense_ssRNA_viral_sequence
-
-    elif mol_id == '5':	# ssRNA(-)	Single-stranded RNA - Negative-sense	5	V	NULL	1500	1600
-        return "http://purl.obolibrary.org/obo/SO_0001200" # negative_sense_ssRNA_viral_sequence
-
-    elif mol_id == '6':	# ssRNA-RT	Single-stranded RNA - Positive-sense - replicates through a DNA intermediate	6	VI	NULL	NULL	NULL
-    elif mol_id == '7':	# dsDNA-RT	Double-stranded DNA - replicates through a single-strand RNA intermediate	7	VII	NULL	NULL	NULL
-    elif mol_id == '8':	# Viroid	Circular Single-stranded RNA	NULL	NULL	NULL	NULL	NULL
-    elif mol_id == '9':	# ssDNA(-)	Single-stranded DNA - Negative-sense	NULL	NULL	NULL	500	600
-    elif mol_id == '10': # ssDNA(+)	Single-stranded DNA - Positive-sense	NULL	NULL	NULL	300	400
-    elif mol_id == '11': # ssDNA(+/-)	Single-stranded DNA - Ambi-sense	NULL	NULL	NULL	200	700
-
-    elif mol_id == '12': # ssRNA(+/-)	Single-stranded RNA - Ambi-sense	NULL	NULL	NULL	1200	1700
-        return "http://purl.obolibrary.org/obo/SO_0001202" # ambisense_ssRNA_viral_sequence
-
-    elif mol_id == '13': # dsDNA; ssDNA	DNA - some taxa double stranded, some taxa single standed	NULL	NULL	NULL	10	900
-    elif mol_id == '14': # ssRNA	Single-stranded RNA	NULL	NULL	Introduced in MSL33 for the viroids	1100	1800
 
 
 
