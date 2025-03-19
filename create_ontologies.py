7ed7214fc4af4ec50ed7d7361557febfaa3f6c53
James McLaughlin <james@mclgh.net>
2025-03-19 15:50:55 +0000

    update script

--- create_ontologies.py
+++ create_ontologies.py
@@ -21,6 +21,14 @@ SOURCE = "http://purl.org/dc/terms/source"
 REPLACES = "http://purl.org/dc/terms/replaces"
 REPLACED_BY = "http://purl.org/dc/terms/isReplacedBy"
 EDITOR_NOTE = "http://purl.obolibrary.org/obo/IAO_0000116"
+IDENTIFIER = "http://purl.org/dc/terms/identifier"
+
+
+# taxnode_id: Taxon within a specific release
+# ictv_id: Taxon across releases
+# 
+# All ictv_ids are taxnode_ids but not all taxnode_ids are ictv_ids
+# 
 
 def main():
 
@@ -32,25 +40,28 @@ def main():
     isolates = pd.read_csv('data/species_isolates.utf8.txt', sep='\t', dtype=str)
 
     releases = nodes[(nodes['level_id'] == '100') & (nodes['name'] != 'empty_tree')]
-
-    prefix = 'https://ictv.global/taxonomy/taxondetails?taxnode_id='
   
     ontologies = []
 
+    taxnode_id_to_ictv_id = {}
+    for node in nodes.itertuples():
+        if node.taxnode_id in taxnode_id_to_ictv_id:
+            raise Exception(f'Taxnode ID {node.ictv_id} already exists')
+        taxnode_id_to_ictv_id[node.taxnode_id] = node.ictv_id
+
+    latest_release = releases['msl_release_num'].max()
+
     for release in releases.itertuples():
 
-        if release.name != '2023' and release.name != '2022':
+        if int(release.msl_release_num) < 38:
             continue
 
         print(f'Creating ontology for ICTV release {release.name}')
 
-        taxnode_id_to_ictv_id = {}
+        msl_id = 'MSL' + release.msl_release_num
+        ontology_iri = f'http://ictv.global/id/{msl_id}'
 
-        id = 'ictv_' + release.name
-        release_num = release.msl_release_num
-        ontology_iri = f'{prefix}{id}'
-
-        nodes_in_release = nodes[(nodes['msl_release_num'] == release_num) & (nodes['level_id'] != '100')]
+        nodes_in_release = nodes[(nodes['msl_release_num'] == release.msl_release_num) & (nodes['level_id'] != '100')]
 
         g = rdflib.Graph()
         ontologies.append(g)
@@ -58,16 +69,11 @@ def main():
         g.add((URIRef(ontology_iri), RDF.type, OWL.Ontology))
         g.add((URIRef(ontology_iri), RDFS.label, Literal("ICTV Taxonomy (" + release.name + ")")))
         g.add((URIRef(ontology_iri), RDFS.comment, Literal("International Committee on Taxonomy of Viruses (ICTV)")))
-        g.add((URIRef(ontology_iri), FOAF.homepage, URIRef("https://ictv.global/")))
-        g.add((URIRef(ontology_iri), OWL.versionInfo, Literal(str(release_num))))
+        g.add((URIRef(ontology_iri), FOAF.homepage, URIRef("http://ictv.global/")))
+        g.add((URIRef(ontology_iri), OWL.versionInfo, Literal(str(release.msl_release_num))))
 
         for node in nodes_in_release.itertuples():
-            class_iri = URIRef(prefix + node.ictv_id)
-
-            if node.taxnode_id in taxnode_id_to_ictv_id:
-                raise Exception(f'Taxnode ID {node.ictv_id} already exists')
-            taxnode_id_to_ictv_id[node.taxnode_id] = node.ictv_id
-            # print(class_iri)
+            class_iri = URIRef(f'http://ictv.global/id/{msl_id}/ICTV{node.ictv_id}')
 
             replacements = delta[delta['prev_taxid'] == node.taxnode_id]
 
@@ -81,42 +87,45 @@ def main():
                 is_new = replacements['is_new'].value_counts().get('1', 0) > 0
                 is_deleted = replacements['is_deleted'].value_counts().get('1', 0) > 0
 
-                # g.add((class_iri, OWL.deprecated, Literal('true', datatype=URIRef('http://www.w3.org/2001/XMLSchema#boolean'))))
+                # an ancestor changed but I didn't necessarily change
+                is_lineage_updated = replacements['is_lineage_updated'].value_counts().get('1', 0) > 0
 
-                for replacement in replacements.itertuples():
-                    if not pd.isna(replacement.new_taxid):
-                        g.add((class_iri, URIRef(TERM_REPLACED_BY), URIRef(prefix+replacement.new_taxid)))
-                        g.add((class_iri, URIRef(REPLACED_BY), URIRef(prefix+replacement.new_taxid)))
-                        g.add((URIRef(prefix+replacement.new_taxid), URIRef(REPLACES), class_iri))
+                g.add((class_iri, OWL.deprecated, Literal('true', datatype=URIRef('http://www.w3.org/2001/XMLSchema#boolean'))))
+
+                replacement_iris = [f'http://ictv.global/id/MSL{replacement.msl}/ICTV{taxnode_id_to_ictv_id[replacement.new_taxid]}' for replacement in replacements.itertuples() if not pd.isna(replacement.new_taxid)]
+
+                for replacement_iri in replacement_iris:
+                    g.add((class_iri, URIRef(TERM_REPLACED_BY), URIRef(replacement_iri)))
+                    g.add((class_iri, URIRef(REPLACED_BY), URIRef(replacement_iri)))
 
                 if is_new:
                     year = replacements['new_taxid'][:4]
                     g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"New in " + year)))
                 elif is_merged:
-                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Merged into {', '.join(replacements['new_taxid'].values)}")))
+                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Merged into {', '.join(replacement_iris)}")))
                     g.add((class_iri, URIRef(OBSOLESCENCE_REASON), URIRef(TERMS_MERGED)))
                 elif is_split:
-                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Split into {', '.join(replacements['new_taxid'].values)}")))
+                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Split into {', '.join(replacement_iris)}")))
                     g.add((class_iri, URIRef(OBSOLESCENCE_REASON), URIRef(TERM_SPLIT)))
                 elif is_moved:
-                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Moved to {', '.join(replacements['new_taxid'].values)}")))
+                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Moved to {', '.join(replacement_iris)}")))
                 elif is_promoted:
-                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Promoted, see {', '.join(replacements['new_taxid'].values)}")))
+                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Promoted, see {', '.join(replacement_iris)}")))
                 elif is_demoted:
-                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Demoted, see {', '.join(replacements['new_taxid'].values)}")))
+                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Demoted, see {', '.join(replacement_iris)}")))
                 elif is_renamed:
-                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Renamed, see {', '.join(replacements['new_taxid'].values)}")))
+                    g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Renamed, see {', '.join(replacement_iris)}")))
                 elif is_deleted:
                     g.add((class_iri, URIRef(EDITOR_NOTE), Literal(f"Deleted")))
 
             g.add((class_iri, RDF.type, OWL.Class))
             g.add((class_iri, RDFS.label, Literal(node.name)))
-            g.add((class_iri, URIRef("http://ictv.global/ictv_id"), Literal(node.ictv_id)))
-            g.add((class_iri, OWL.versionInfo, Literal(str(release_num))))
+            g.add((class_iri, URIRef(IDENTIFIER), Literal("ICTV" + node.ictv_id)))
+            g.add((class_iri, OWL.versionInfo, Literal(str(msl_id))))
 
             if node.parent_id != release.ictv_id:
                 if node.parent_id in taxnode_id_to_ictv_id:
-                    g.add((class_iri, RDFS.subClassOf, URIRef(f'{prefix}{taxnode_id_to_ictv_id[node.parent_id]}')))
+                    g.add((class_iri, RDFS.subClassOf, URIRef(f'http://ictv.global/id/{msl_id}/ICTV{taxnode_id_to_ictv_id[node.parent_id]}')))
             g.add((class_iri, RDFS.isDefinedBy, URIRef(ontology_iri)))
 
             if not pd.isna(node.abbrev_csv):
@@ -130,7 +139,7 @@ def main():
             taxnode_isolates = isolates[isolates['taxnode_id'] == node.taxnode_id]
 
             for isolate in taxnode_isolates.itertuples():
-                isolate_iri = f'https://ictv.global/isolates#isolate_{isolate.isolate_id}'
+                isolate_iri = f'http://ictv.global/id/VMR{isolate.isolate_id}'
                 g.add((URIRef(isolate_iri), RDF.type, OWL.NamedIndividual))
                 g.add((URIRef(isolate_iri), RDF.type, class_iri))
                 g.add((URIRef(isolate_iri), RDFS.isDefinedBy, URIRef(ontology_iri)))
@@ -150,11 +159,40 @@ def main():
                         g.add((class_iri, SKOS.narrowMatch, Literal("refseq:" + xref.strip())))
 
 
+    g_all = rdflib.Graph()
+    for g in ontologies:
+        g_all += g
+
+    # For all classes that aren't deprecated yet
+    # If they are not from the latest ICTV version, add a replaced_by to their latest version
+    # and mark them as deprecated
+    for g in ontologies:
+        ontology = g.value(predicate=RDF.type, object=OWL.Ontology)
+        ontology_id = ontology.split('/')[-1]
+        if ontology_id == 'MSL' + latest_release:
+            continue
+        for c in g.subjects(predicate=RDF.type, object=OWL.Class):
+            if len(list(g.triples((c, OWL.deprecated, Literal('true', datatype=URIRef('http://www.w3.org/2001/XMLSchema#boolean')))))) == 0:
+                ictv_id = g.value(subject=c, predicate=URIRef(IDENTIFIER))
+                latest_version = sorted(list(g_all.subjects(predicate=URIRef(IDENTIFIER), object=ictv_id)))[-1]
+                if latest_version != c:
+                    g.add((URIRef(c), OWL.deprecated, Literal('true', datatype=URIRef('http://www.w3.org/2001/XMLSchema#boolean'))))
+                    g.add((URIRef(c), URIRef(REPLACED_BY), latest_version))
+                    g.add((URIRef(c), URIRef(TERM_REPLACED_BY), latest_version))
+
+    # For all of the replaced_by, add the inverse triple "replaces"
+    for g in ontologies:
+        for c in g.subjects(predicate=RDF.type, object=OWL.Class):
+            for replaces in g_all.subjects(predicate=URIRef(REPLACED_BY), object=c):
+                g.add((URIRef(c), URIRef(REPLACES), replaces))
+
+    all_g_merged = rdflib.Graph()
+
     for g_orig in ontologies:
         g = rdflib.Graph()
         g += g_orig
         ontology = g.value(predicate=RDF.type, object=OWL.Ontology)
-        ontology_id = ontology.split('=')[-1]
+        ontology_id = ontology.split('/')[-1]
         version = g.value(subject=ontology, predicate=OWL.versionInfo)
         print(f'Building final ontology for {ontology_id} (version = {version})')
         for other_g in ontologies:
@@ -163,8 +201,8 @@ def main():
             if other_version < version:
                 print(f'- merging {other_version} into {version}')
                 for c in g.subjects(predicate=RDF.type, object=OWL.Class):
-                    ictv_id = g.value(subject=c, predicate=URIRef("http://ictv.global/ictv_id"))
-                    prev_versions = list(other_g.subjects(predicate=URIRef("http://ictv.global/ictv_id"), object=ictv_id))
+                    ictv_id = g.value(subject=c, predicate=URIRef(IDENTIFIER))
+                    prev_versions = list(other_g.subjects(predicate=URIRef(IDENTIFIER), object=ictv_id))
                     cur_name = g.value(subject=c, predicate=RDFS.label)
                     for prev_version in prev_versions:
                         prev_name = other_g.value(subject=prev_version, predicate=RDFS.label)
@@ -177,12 +215,13 @@ def main():
                                 g.add((stmt, OWL.annotatedProperty, URIRef(SYNONYM)))
                                 g.add((stmt, OWL.annotatedTarget, Literal(name)))
                                 g.add((stmt, URIRef(SYNONYM_TYPE), URIRef(PREVIOUS_NAME)))
-                                g.add((stmt, RDFS.isDefinedBy, other_ontology))
-                other_g_copy = rdflib.Graph()
-                other_g_copy += other_g
-                other_g_copy.remove((URIRef(ontology_iri), RDF.type, OWL.Ontology))
-                other_g_copy.remove((next(other_g_copy.subjects(RDF.type, OWL.Ontology), None), None, None))
-                g += other_g_copy
+                                # g.add((stmt, RDFS.isDefinedBy, other_ontology))
+                                g.add((stmt, OWL.versionInfo, Literal( other_ontology.split('/')[-1])))
+                #other_g_copy = rdflib.Graph()
+                #other_g_copy += other_g
+                #other_g_copy.remove((URIRef(ontology_iri), RDF.type, OWL.Ontology))
+                #other_g_copy.remove((next(other_g_copy.subjects(RDF.type, OWL.Ontology), None), None, None))
+                #g += other_g_copy
 
         owl_filename = 'out/' + ontology_id + '.owl.ttl'
         print(f'Saving OWL file ' + owl_filename)
@@ -192,25 +231,35 @@ def main():
         g.bind('oio', 'http://www.geneontology.org/formats/oboInOwl#')
         g.bind('omo', 'http://purl.obolibrary.org/obo/OMO_')
         g.bind('dcterms', 'http://purl.org/dc/terms/')
-        g.bind('ictv', prefix)
+        # g.bind('ictv', 'http://ictv.global/')
         g.serialize(owl_filename, format="ttl")
+        all_g_merged += g
 
-    owl_files = [f for f in os.listdir('out') if f.endswith('.owl.ttl')]
+    all_g_merged.serialize('out/ictv_all_versions.owl.ttl', format="ttl")
+
+    owl_files = [f for f in os.listdir('out') if f.startswith('MSL') and f.endswith('.owl.ttl')]
     ols_config = {
-        'ontologies':[
-            {
-                "id": "evora",
-                "ontology_purl": "https://raw.githubusercontent.com/EVORA-project/evora-ontology/refs/heads/main/models/owl/evora_ontology.owl.ttl"
-            }
-         ] + json.load(open('supporting_ontologies.json'))['ontologies'] + list(map(lambda f: {
+        'ontologies': json.load(open('supporting_ontologies.json'))['ontologies'] + list(map(lambda f: {
             'id': f.split('.')[0],
             'ontology_purl': 'file:///opt/dataload/out/' + f,
-            'preferredPrefix': prefix,
-            'base_uri': ["https://ictv.global/taxonomy/taxondetails?taxnode_id=","https://ictv.global/isolates#"]
+            'preferredPrefix': "ICTV",
+            'base_uri': ['http://ictv.global/id/' + f.split('.')[0] + '/']
         }, owl_files))
     }
     with open('ols_config.json', 'w') as f:
         f.write(json.dumps(ols_config, indent=2))
+
+    ols_config_combined = {
+         'ontologies': json.load(open('supporting_ontologies.json'))['ontologies'] + [{
+            'id': 'ictv',
+            'ontology_purl': 'file:///opt/dataload/out/ictv_all_versions.owl.ttl',
+            'preferredPrefix': "ICTV",
+            'base_uri': list(map(lambda f: 'http://ictv.global/id/' + f.split('.')[0] + '/', owl_files))
+        }]
+    }
+
+    with open('ols_config_combined.json', 'w') as f:
+        f.write(json.dumps(ols_config_combined, indent=2))
 
 def add_xrefs(g, class_iri, field, prefix):
     entries = re.split(';|,', field)
